865975eff086ff60ef164f8842d1529603c393a2
michael burton <michaelburton@michaels-MacBook-Pro.local>
2023-09-15 12:05:19 -0700

    Adds support for showing sources in console output for the grants cmd

--- vendor/github.com/conductorone/baton-sdk/pb/c1/reader/v2/grant.pb.validate.go
+++ vendor/github.com/conductorone/baton-sdk/pb/c1/reader/v2/grant.pb.validate.go
@@ -350,6 +350,35 @@ func (m *GrantsReaderServiceListGrantsForEntitlementRequest) validate(all bool) 
 		}
 	}
 
+	if all {
+		switch v := interface{}(m.GetPrincipalId()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, GrantsReaderServiceListGrantsForEntitlementRequestValidationError{
+					field:  "PrincipalId",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, GrantsReaderServiceListGrantsForEntitlementRequestValidationError{
+					field:  "PrincipalId",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetPrincipalId()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return GrantsReaderServiceListGrantsForEntitlementRequestValidationError{
+				field:  "PrincipalId",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if m.GetPageSize() != 0 {
 
 		if m.GetPageSize() > 250 {
