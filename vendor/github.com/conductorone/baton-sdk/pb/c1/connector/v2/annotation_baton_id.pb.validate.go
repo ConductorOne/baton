15c693d4782ba1ccb6da302e8a4afbd4aae4f4bc
Lauren Leach <lauren.leach@conductorone.com>
2025-04-02 17:36:46 -0700

    bump baton-sdk version to v0.2.90

--- vendor/github.com/conductorone/baton-sdk/pb/c1/connector/v2/annotation_baton_id.pb.validate.go
+++ vendor/github.com/conductorone/baton-sdk/pb/c1/connector/v2/annotation_baton_id.pb.validate.go
@@ -247,6 +247,110 @@ var _ interface {
 	ErrorName() string
 } = ExternalResourceMatchAllValidationError{}
 
+// Validate checks the field values on ExternalResourceMatchID with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the first error encountered is returned, or nil if there are no violations.
+func (m *ExternalResourceMatchID) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on ExternalResourceMatchID with the
+// rules defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// ExternalResourceMatchIDMultiError, or nil if none found.
+func (m *ExternalResourceMatchID) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *ExternalResourceMatchID) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for Id
+
+	if len(errors) > 0 {
+		return ExternalResourceMatchIDMultiError(errors)
+	}
+
+	return nil
+}
+
+// ExternalResourceMatchIDMultiError is an error wrapping multiple validation
+// errors returned by ExternalResourceMatchID.ValidateAll() if the designated
+// constraints aren't met.
+type ExternalResourceMatchIDMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m ExternalResourceMatchIDMultiError) Error() string {
+	msgs := make([]string, 0, len(m))
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m ExternalResourceMatchIDMultiError) AllErrors() []error { return m }
+
+// ExternalResourceMatchIDValidationError is the validation error returned by
+// ExternalResourceMatchID.Validate if the designated constraints aren't met.
+type ExternalResourceMatchIDValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e ExternalResourceMatchIDValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e ExternalResourceMatchIDValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e ExternalResourceMatchIDValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e ExternalResourceMatchIDValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e ExternalResourceMatchIDValidationError) ErrorName() string {
+	return "ExternalResourceMatchIDValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e ExternalResourceMatchIDValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sExternalResourceMatchID.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = ExternalResourceMatchIDValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = ExternalResourceMatchIDValidationError{}
+
 // Validate checks the field values on BatonID with the rules defined in the
 // proto definition for this message. If any rules are violated, the first
 // error encountered is returned, or nil if there are no violations.
